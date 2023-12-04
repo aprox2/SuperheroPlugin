1f938b763af7988f3d2c556bc54cf531e14436f1
aprox2 <edge15831@gmail.com>
2023-12-05 00:21:42 +0200

    Cleanup

--- SuperheroHooks.cs
+++ SuperheroHooks.cs
@@ -2,43 +2,42 @@ using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 
-namespace SuperheroPlugin
+namespace SuperheroPlugin;
+
+public partial class SuperheroPlugin
 {
-	public partial class SuperheroPlugin
+	[GameEventHandler(HookMode.Post)]
+	public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
 	{
-		[GameEventHandler(HookMode.Post)]
-		public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
-		{
-			SuperheroTasks.printLevelToAllPlayers(this.SuperheroPlayers);
-			SuperheroTasks.activatePlayerOnRoundStartAbilities(this.SuperheroPlayers);
-			return HookResult.Continue;
-		}
-
-		[GameEventHandler(HookMode.Post)]
-		public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info) 
-		{
-
-			if (@event.Userid == null 
-				|| @event.Attacker == null 
-				|| @event.Attacker.Handle == @event.Userid.Handle
-				|| @event.Attacker.IsBot == true
-			) { return HookResult.Continue; }
-
-            SuperheroPlayer attackerPlayer = SuperheroPlayers[@event.Attacker.Handle];
-            bool isHeadshot = @event.Headshot;
-			int xpToAddAttacker = this.XpPerKill;
-			xpToAddAttacker = (int) (isHeadshot ? xpToAddAttacker * this.XpHeadshotMultiplier : xpToAddAttacker);
-			attackerPlayer.addXp(xpToAddAttacker);
-
-			// TEMP FIX
-			if (@event.Assister == null	|| @event.Assister.IsBot == true) { return HookResult.Continue; }
-
-			SuperheroPlayer assisterPlayer = SuperheroPlayers[@event.Assister.Handle];
-			if ( assisterPlayer == null ) { return HookResult.Continue; }
-			int xpToAddAssister = (int) (this.XpPerKill * this.XpAssistMultiplier);
-			assisterPlayer.addXp(xpToAddAssister);
-
-			return HookResult.Continue;
-        }
+		SuperheroTasks.printLevelToAllPlayers(this.SuperheroPlayers);
+		SuperheroTasks.activatePlayerOnRoundStartAbilities(this.SuperheroPlayers);
+		return HookResult.Continue;
 	}
+
+	[GameEventHandler(HookMode.Post)]
+	public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info) 
+	{
+
+		if (@event.Userid == null 
+			|| @event.Attacker == null 
+			|| @event.Attacker.Handle == @event.Userid.Handle
+			|| @event.Attacker.IsBot == true
+		) { return HookResult.Continue; }
+
+        SuperheroPlayer attackerPlayer = SuperheroPlayers[@event.Attacker.Handle];
+        bool isHeadshot = @event.Headshot;
+		int xpToAddAttacker = this.XpPerKill;
+		xpToAddAttacker = (int) (isHeadshot ? xpToAddAttacker * this.XpHeadshotMultiplier : xpToAddAttacker);
+		attackerPlayer.addXp(xpToAddAttacker);
+
+		// TEMP FIX
+		if (@event.Assister == null	|| @event.Assister.IsBot == true) { return HookResult.Continue; }
+
+		SuperheroPlayer assisterPlayer = SuperheroPlayers[@event.Assister.Handle];
+		if ( assisterPlayer == null ) { return HookResult.Continue; }
+		int xpToAddAssister = (int) (this.XpPerKill * this.XpAssistMultiplier);
+		assisterPlayer.addXp(xpToAddAssister);
+
+		return HookResult.Continue;
+    }
 }
--- SuperheroTasks.cs
+++ SuperheroTasks.cs
@@ -1,56 +1,54 @@
 using System.Runtime.CompilerServices;
 
-namespace SuperheroPlugin
+namespace SuperheroPlugin;
+
+public static class SuperheroTasks
 {
 
-    public static class SuperheroTasks
-    {
-
-        public static void printLevelToAllPlayers(Dictionary<IntPtr, SuperheroPlayer> shPlayers)
-        { 
-            foreach (var player in shPlayers) 
-            {
-                if (player.Value == null) { continue; }
-                SuperheroTasks.printLevel(player.Value);
-            }
-        }
-
-        public static void activatePlayerOnRoundStartAbilities(Dictionary<IntPtr, SuperheroPlayer> shPlayers)
+    public static void printLevelToAllPlayers(Dictionary<IntPtr, SuperheroPlayer> shPlayers)
+    { 
+        foreach (var player in shPlayers) 
         {
-            Task.Run(() =>
-            {
-                foreach (var shPlayer in shPlayers)
-                {
-                    if (shPlayer.Value == null || shPlayer.Value.Player == null) { continue; }
-                    shPlayer.Value.activateRoundStartAbilities();
-                }
-            });
+            if (player.Value == null) { continue; }
+            SuperheroTasks.printLevel(player.Value);
         }
-
-        public static void printLevel(SuperheroPlayer shPlayer, int firstDelay=2000)
-        {
-            if (shPlayer == null || shPlayer.Player == null) { return; }
-
-            Task.Run(async () =>
-            {
-                await Task.Delay(firstDelay);
-
-                var timer = new System.Timers.Timer();
-                timer.Start();
-                timer.Interval = 500;
-                timer.Elapsed += (sender, e) =>
-                {
-                    shPlayer.Player.PrintToCenter(
-                        StringResources.LevelStatsMessage(
-                            shPlayer.currentLevel, shPlayer.currentXp,
-                            shPlayer.levelUpXp, shPlayer.availableAbilityPoints)
-                    );
-                };
-                await Task.Delay(12000);
-                timer.Stop();
-                timer.Dispose();
-            });
-        }
-
     }
+
+    public static void activatePlayerOnRoundStartAbilities(Dictionary<IntPtr, SuperheroPlayer> shPlayers)
+    {
+        Task.Run(() =>
+        {
+            foreach (var shPlayer in shPlayers)
+            {
+                if (shPlayer.Value == null || shPlayer.Value.Player == null) { continue; }
+                shPlayer.Value.activateRoundStartAbilities();
+            }
+        });
+    }
+
+    public static void printLevel(SuperheroPlayer shPlayer, int firstDelay=2000)
+    {
+        if (shPlayer == null || shPlayer.Player == null) { return; }
+
+        Task.Run(async () =>
+        {
+            await Task.Delay(firstDelay);
+
+            var timer = new System.Timers.Timer();
+            timer.Start();
+            timer.Interval = 500;
+            timer.Elapsed += (sender, e) =>
+            {
+                shPlayer.Player.PrintToCenter(
+                    StringResources.LevelStatsMessage(
+                        shPlayer.currentLevel, shPlayer.currentXp,
+                        shPlayer.levelUpXp, shPlayer.availableAbilityPoints)
+                );
+            };
+            await Task.Delay(12000);
+            timer.Stop();
+            timer.Dispose();
+        });
+    }
+
 }
